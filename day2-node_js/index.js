85ac9abec57351e85b97003866860d3c1de31574
DHARSHINIKANDASAMY <dharshinik.23cse@kongu.edu>
2025-12-08 21:16:46 +0530

    first commit

--- day2-node_js/index.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello, Node.js!");
-
-let arr=[10,20,30,40,50];
-arr.map((value)=>{
-    console.log(value);
-});
--- day2-node_js/samplefile.js
+++
@@ -1,4 +0,0 @@
-let fs=require('fs');
-console.log(fs);
-
-console.log(fs.exists('samplefile.js'));
